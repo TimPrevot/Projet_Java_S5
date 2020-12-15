b46f4f95c743482a6240214d653a32aa7160005d
deadbang3r <locopok33185@gmail.com>
2020-12-15 16:22:41 +0100

    Ajout des classes Joueur et Territoire

--- src/Projet/Joueur.java
+++ src/Projet/Joueur.java
@@ -1,4 +1,37 @@
 package Projet;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Joueur {
+    private static int count = 0;
+    private int ID = 0;
+    private ArrayList<String> listeTerritoires;
+
+    // Constructeur
+    public Joueur() {
+    }
+
+    // Getters et setters
+    public void setID() {
+        this.ID = count++;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public void attaquerTerritoire() {
+        Scanner scanner = new Scanner(System.in);
+        String territoireAttaquant = scanner.next();
+        String territoireAttaque = scanner.next();
+    }
+
+    public void terminerTour() {
+        /* A implémenter */
+    }
 }
