819eccd7917dbc6ea319ab266781ad0f5fb98cca
Eric-Dad <e.prevot@fondationccv.org>
2020-12-26 18:12:53 +0100

    Test EPR 2

--- src/Projet/CarteTest.java
+++ src/Projet/CarteTest.java
@@ -2,11 +2,8 @@ package Projet;
 
 public class CarteTest {
     public static void main(String[] args) {
-        Territoire t1 = new Territoire();
-        System.out.println(t1.toString());
-        Carte carte = new Carte();
-        carte.addTerritoire(t1);
-        System.out.println(carte.toString());
+
+        System.out.println("yo epr");
 
     }
 }
